7376928f788f7bbf89a0f3e084ef13c8b9aeeb79
achille1017 <achillemarseille@gmail.com>
2023-08-24 19:22:47 +0200

    nft metadatas

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ def mint_nft(address):
         'token_amount': 1,
         'ipfs_uri': ipfs_uri,
     }
+    print(ipfs_uri)
     mint_call_resp = requests.post(
         f"{DOMAIN}/fa2-token/",
         data=fa2_token_data,
